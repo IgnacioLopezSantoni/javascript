2a688ff8dfd0e673c6cdecddfe23c8c4391f23de
Nacho lopez santoni <nacholopezsantoni@gmail.com>
2022-06-27 11:54:59 -0300

    desafio complementario

---
+++ main.js
@@ -0,0 +1,29 @@
+/*let nombre = prompt ("ingresu su nombre")
+let precio = 0
+alert ("buen dia " + nombre)
+
+let corte = prompt( "ingrese que tipo de corte quiere " + " opcion 1 femenino , opcion 2 masculino" )
+
+while(corte !=1 && corte !=2){
+
+    alert("el numero ingresado es incorrecto")
+    corte = prompt( "ingrese que tipo de corte quiere " + " opcion 1 femenino , opcion 2 masculino" )
+}
+
+if(corte ==1){
+    precio= 500
+} else(precio= 900 )
+ alert ( "el precio del corte es " + precio)
+ */
+ 
+
+
+    let nombre = prompt ("poneme un nombre")
+    alert( "vaya que nombre me has dado " + nombre)
+let accion = prompt ("que queres que haga?, (comer, dormir, dar amor)")
+
+    
+
+
+
+
